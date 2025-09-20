384b84ce504450a78e2fccfe4a5ba6f59b72d532
chrizxzayax <chmolina629@hotmail.com>
2025-09-19 19:14:05 -0700

    update commit

--- lab11.cpp
+++ lab11.cpp
@@ -19,4 +19,38 @@ struct Student1 {
             classes = nullptr;
         }
     }
+
+    //forgot to add a destructor to free memory
+    ~Student1(){
+        delete[] classes;
+    }
 };
+int main () {
+    int studentsnum;
+    cout << "Enter number of students who are enrolling: ";
+    cin >> studentsnum;
+
+    cin.ignore(); // to ignore the newline character after the integer input
+    Student1* students = new Student1[studentsnum];
+
+    for (int i = 0; i < studentsnum; i++) {
+        cout << "Enter name of student " << (i + 1) << ": ";
+        getline(cin, students[i].nam);
+
+        cout << "Enter ID of student " << (i + 1) << ": ";
+        cin >> students[i].id;
+
+        cout << "Enter number of courses for student " << (i + 1) << ": ";
+        cin >> students[i].coursesnum;
+        cin.ignore(); // to ignore the newline character after the integer input
+
+        // allocate memory for classes
+        students[i] = Student1(students[i].coursesnum);
+
+        for (int j = 0; j < students[i].coursesnum; j++) {
+            cout << "Enter name of course " << (j + 1) << " for student " << (i + 1) << ": ";
+            getline(cin, students[i].classes[j]);
+        }
+    }
+
+}
