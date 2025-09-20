aeb2f92a7b922847a99d5815330d8eecd971b024
chrizxzayax <chmolina629@hotmail.com>
2025-09-19 19:24:52 -0700

    update commit2

--- lab11.cpp
+++ lab11.cpp
@@ -25,6 +25,7 @@ struct Student1 {
         delete[] classes;
     }
 };
+
 int main () {
     int studentsnum;
     cout << "Enter number of students who are enrolling: ";
@@ -34,23 +35,25 @@ int main () {
     Student1* students = new Student1[studentsnum];
 
     for (int i = 0; i < studentsnum; i++) {
-        cout << "Enter name of student " << (i + 1) << ": ";
+        cout << "\nInput data for the Student #" << (i + 1) << endl;
+        cout << "Enter name of student: ";
         getline(cin, students[i].nam);
-
-        cout << "Enter ID of student " << (i + 1) << ": ";
+        cout << "Enter ID number of student: ";
         cin >> students[i].id;
-
-        cout << "Enter number of courses for student " << (i + 1) << ": ";
+        cout << "Enter number of courses enrolled for student: ";
         cin >> students[i].coursesnum;
         cin.ignore(); // to ignore the newline character after the integer input
 
-        // allocate memory for classes
-        students[i] = Student1(students[i].coursesnum);
+        // allocate memory for classes based on coursesnum
+        students[i].classes = new string[students[i].coursesnum];
 
         for (int j = 0; j < students[i].coursesnum; j++) {
-            cout << "Enter name of course " << (j + 1) << " for student " << (i + 1) << ": ";
+            cout << "  Enter name of course #" << (j + 1) << ": ";
             getline(cin, students[i].classes[j]);
         }
     }
+
+    //output will display all students and their info, curses etc
+    cout 
 
 }
